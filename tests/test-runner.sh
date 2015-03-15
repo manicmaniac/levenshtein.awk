cb6277dddf69d5fc81c0c789af79f5d75691b8f3
Ryosuke Ito <rito.0305@gmail.com>
2015-03-16 01:01:47 +0900

    CHANGE: Test runner for bin/levenshtein

--- tests/test-runner.sh
+++ tests/test-runner.sh
@@ -6,6 +6,7 @@ cd "$(dirname "$0")"
 assert_awk='../vendors/assert.awk/assert.awk'
 levenshtein_awk='../levenshtein.awk'
 levenshtein_test_awk='./levenshtein-test.awk'
+levenshtein_test_sh='./levenshtein-test.sh'
 
 if [ ! -f "$assert_awk" ]; then
     echo "error: $assert_awk does not exist." >&2
@@ -20,5 +21,14 @@ if [ ! -f "$levenshtein_test_awk" ]; then
     exit 1
 fi
 
+echo "# $levenshtein_test_awk"
 awk -f "$assert_awk" -f "$levenshtein_awk" -f "$levenshtein_test_awk"
+if [ $? -eq 0 ]; then
+    echo 'Test succeeded.'
+fi
+
+echo
+
+echo "# $levenshtein_test_sh"
+sh "$levenshtein_test_sh"
 
